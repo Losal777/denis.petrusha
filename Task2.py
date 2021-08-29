f069dd30f5528be840e33c52f4b6c24dbcf39734
Losal777 <88239586+Losal777@users.noreply.github.com>
2021-08-29 15:07:02 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -5,3 +5,20 @@
 # Если нужная буква, то прибавить в счётчик.
 # Вернуть счётчик.
 #* Решить в одну строку (без компрехеншена).
+
+def letter(text):
+    new_list = []
+
+    for a in text:
+        new_list.append((a, len(set(a))))
+
+    new_list = [(a, len(set(a))) for a in text]
+
+    new_list = sorted(new_list, key=lambda x: x[-1], reverse=True)[:3]
+    print(new_list)
+    return [w[0] for w in new_list]
+
+if __name__ == '__main__':
+    words = ['a', 'London is a capital of Great Britain']
+
+    print(letter(words))
